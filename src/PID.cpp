c7624ac8d5c249834e4925c6a0dbf082d80c4331
Sankar Rengarajan <mulshankar@gmail.com>
2018-01-16 09:32:58 -0600

    first rev code test on sim

--- src/PID.cpp
+++ src/PID.cpp
@@ -11,11 +11,25 @@ PID::PID() {}
 PID::~PID() {}
 
 void PID::Init(double Kp, double Ki, double Kd) {
+
+	this->Kp=Kp;
+	this->Ki=Ki;
+	this->Kd=Kd;
+	
+	p_error=0;
+	i_error=0;
+	d_error=0;
 }
 
 void PID::UpdateError(double cte) {
+
+	p_error=cte;
+	//i_error=0;
+	//d_error=0;
 }
 
 double PID::TotalError() {
+
+	return (Kp*p_error+Ki*i_error+Kd*d_error);
 }
 
