2f9193eba65dd0570c5bdf611888912481d6dc6c
Sankar Rengarajan <mulshankar@gmail.com>
2018-01-17 07:12:58 -0600

    tune

--- src/main.cpp
+++ src/main.cpp
@@ -36,7 +36,7 @@ int main()
   // TODO: Initialize the pid variable.
   
   double Kp_init=-0.05;
-  double Ki_init=-0.001;//-0.02;
+  double Ki_init=-0.0001;//-0.02;
   double Kd_init=0.0;//0.02;
   
   pid.Init(Kp_init,Ki_init,Kd_init);
